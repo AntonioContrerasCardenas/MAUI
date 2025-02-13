adaf37d9d2a2dc5b67a25118623c34cc38f7f967
Antonio <aconcar181@g.educaand.es>
2025-02-13 16:41:24 +0100

    Add AnimalDataTemplateSelector and update HomePage layout; implement dynamic data templates for adopted and non-adopted animals

---
+++ MascotaApp/Selectors/AnimalDataTemplateSelector.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MascotaApp.MVVM.Model;
+
+namespace MascotaApp.Selectors
+{
+    public class AnimalDataTemplateSelector : DataTemplateSelector
+    {
+        protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+        {
+            var animal = item as Animal;
+            if (animal.IsAdopted)
+            {
+                Application.Current.Resources.TryGetValue("AdoptadoStyle", out var adoptadoStyle);
+                return adoptadoStyle as DataTemplate;
+            }else
+            {
+                Application.Current.Resources.TryGetValue("NodoptadoStyle", out var adoptadoStyle);
+                return adoptadoStyle as DataTemplate;
+            }
+            return new DataTemplate();
+        }
+    }
+}
